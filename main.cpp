64cbc86f227c2772b21ba8786541975df1da889b
osinoko <k023g0114@g.neec.ac.jp>
2025-02-12 11:48:13 +0900

    プロジェクト ファイルを追加します。

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <Novice.h>
 #include "Scene.h"
 
-const char kWindowTitle[] = "LE2*_**_***_***";
+const char kWindowTitle[] = "LE2D_08_サクライ_ショウセイ";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
